d28fb7f24e970b84f32cb2b2f1702b783e0743f9
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-10-05 16:33:53 -0400

    basic probabilistic trendline

---
+++ Main.py
@@ -0,0 +1,13 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+path = 0
+xs = []
+path_length = 10000
+for i in range(path_length):
+    step = np.random.choice([1,-1], p=[0.5,0.5])
+    path += step
+    xs.append(path)
+
+plt.plot(xs)
+plt.show()
